7abadccd424f4f42dbee5598d86f1a998c111f75
Jaspreet Singh Kohli <70095662+Jaspreetkohli72@users.noreply.github.com>
2022-01-18 13:41:20 +0530

    heroku fix

--- app.py
+++ app.py
@@ -19,8 +19,9 @@ users.append(User(id=2, username='Real Estate', password='secret'))
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
-port = int(os.environ.get('PORT', 33507))
-app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT'))
+    app.run(host='0.0.0.0', port=port)
 
 
 @app.before_request
