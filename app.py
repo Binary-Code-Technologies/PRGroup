570070bafa7bcb076c3a52e46588c2d09634e4d0
Jaspreet Singh Kohli <70095662+Jaspreetkohli72@users.noreply.github.com>
2022-01-18 15:38:22 +0530

    Heroku Fix

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import os
 from flask import Flask, g, redirect, render_template, request, session, url_for
-
+import waitress
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.debug = False
+    port = int(os.environ.get('PORT', 33507))
+    # app.run(host='0.0.0.0', port=port)
+    waitress.serve(app, port=port)
 
 
 class User:
