e8e3e867fc3688b681624d911ac81a1dcfefb816
Jaspreet Singh Kohli <70095662+Jaspreetkohli72@users.noreply.github.com>
2022-01-18 12:52:17 +0530

    Added All Files

---
+++ app.py
@@ -0,0 +1,67 @@
+import os
+from flask import Flask, g, redirect, render_template, request, session, url_for
+
+
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+
+users = []
+users.append(User(id=1, username='Consultancy', password='password'))
+users.append(User(id=2, username='Real Estate', password='secret'))
+
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('profile'))
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+
+@app.route('/profile')
+def profile():
+    if not g.user:
+        return redirect(url_for('login'))
+
+    if g.user.username == 'Consultancy':
+        return render_template('consultancy.html')
+    elif g.user.username == 'Real Estate':
+        return render_template('realEstate.html')
+
+    return render_template('error.html')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
