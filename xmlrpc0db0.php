c7d4d839fe9f8093e2a92c9455b504c64f63f592
Jaspreet Singh Kohli <70095662+Jaspreetkohli72@users.noreply.github.com>
2022-03-01 21:58:01 +0530

    Bug Fixes

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://vinayakjob.com</homePageLink>
+		<homePageLink>https://PR Consultantjob.com</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://vinayakjob.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://vinayakjob.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://vinayakjob.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://vinayakjob.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://vinayakjob.com/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://PR Consultantjob.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://PR Consultantjob.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://PR Consultantjob.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://PR Consultantjob.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://PR Consultantjob.com/wp-json/" />
 			</apis>
 	</service>
 </rsd>
